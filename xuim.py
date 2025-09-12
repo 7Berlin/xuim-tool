63450f9924c21c736b292cebfda271f81387eb5f
7Berlin <jafarzadeha93@gmail.com>
2025-09-12 08:03:27 +0330

    Fix update command

--- xuim.py
+++ xuim.py
@@ -650,9 +650,9 @@ def uninstall_tool():
         exit_code = os.system(
             "bash -c 'bash <(curl -s https://raw.githubusercontent.com/7berlin/xuim-tool/main/uninstall.sh)'"
         )
-        sys.exit(0)
         if exit_code == 0:
             print("✅ Uninstall completed successfully.")
+            sys.exit(0)
         else:
             print(f"❌ Uninstall script failed with exit code {exit_code}")
     except Exception as e:
